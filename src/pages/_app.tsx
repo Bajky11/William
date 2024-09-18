10ad3d55b71608cddf121d48b8eb643bd247fdd0
Bajky11 <lukin.bajer@gmail.com>
2024-09-18 19:05:42 +0200

    deployment fix 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import "../styles/globals.css";
 import type {AppProps} from "next/app";
 import {Provider} from "react-redux";
-import {store} from "@/utils/redux/store";
+import {store} from "../utils/redux/store";
 import {ThemeProvider} from '@mui/material/styles';
 import theme from "@/utils/mui/theme";
 import Navbar from "@/modules/shared/components/Navbar";
