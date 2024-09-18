82269df9d785a3cdf126d47a6c1b82e784a0e857
Bajky11 <lukin.bajer@gmail.com>
2024-09-18 19:17:38 +0200

    deployment fix 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-    // Přidání aliasu
-    config.resolve.alias['@'] = path.resolve('./src');
-    return config;
-  },
 };
 
 export default nextConfig;
