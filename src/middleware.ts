9ddf0f4c8691c627bca0a163662020b769f7f31d
LuBajer <LuBajer@cpas.cz>
2024-10-04 14:49:33 +0200

    Added middleware.ts for login redirecting.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,22 @@
-import {NextResponse} from 'next/server';
-import type {NextRequest} from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-// Funkce middleware
 export default function middleware(req: NextRequest) {
+    const { pathname } = req.nextUrl;
 
+    // Kontrola, jestli uživatel není přihlášen (tvá logika ověřování)
+    const loggedIn = false;
 
-    /*
-    return NextResponse.redirect(new URL('/login', req.url));
-
-    // Získání tokenu nebo stavu přihlášení
-    console.log(req.cookies)
-    const token = req.cookies.get('token'); // Například z cookies, kde máte uložený token
-
-    const {pathname} = req.nextUrl;
-
-    // Pokud není uživatel přihlášený a není na stránce /login
-    if (!token && pathname !== '/login') {
-        // Přesměrujte na /login
-        return NextResponse.redirect(new URL('/login', req.url));
+    // Přesměrování na /login, pokud uživatel není přihlášen a je na kořenové URL
+    if (pathname === '/') {
+        if (!loggedIn) {
+            return NextResponse.redirect(new URL('/login', req.url));
+        }
     }
 
-    // Pokud uživatel přihlášený je nebo je na stránce /login, pokračujte normálně
     return NextResponse.next();
-     */
 }
 
 export const config = {
-    // Určení, které cesty budou middleware zpracovány
-    matcher: ['/'],
+    matcher: ['/'], // Spustí middleware na kořenové URL '/'
 };
